29a4230a823f640383d7fb950ac4c39873c260b2
Your Name <you@example.com>
2023-03-23 21:40:12 +1100

    Added sample carousel and comments -Simone

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,9 @@ const navbarMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
+
+bulmaCarousel.attach('#post_images', {
+    slidesToScroll:1,
+    slidesToShow: 1,
+    loop:true
+})
