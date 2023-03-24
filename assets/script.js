cc71f0b64f461dd44c70d67ae53077d9787d293f
Andrew Evripidou <aevripidou1996@gmail.com>
2023-03-25 09:54:04 +1100

    new api key

--- assets/script.js
+++ assets/script.js
@@ -41,11 +41,35 @@ function getResterauntListData(){
 	
 	fetch('https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156&pageSize=10&pageNumber=1', options)
 		.then(response => response.json())
-		.then(response => console.log(response))
+		.then(data => {
+			test(data);
+		})
 		.catch(err => console.error(err));
 	
 };
 
 googleId();
 getCityId();
-getResterauntListData();
+console.log(getResterauntListData());
+
+function test(array) {
+	console.log(array);
+	const body = document.querySelector("body");
+	for (let i=0; i<array.data.length; i++){
+		const text = document.createElement("p");
+		text.textContent = array.data[i].name;
+		body.appendChild(text);
+		console.log("restaurant" + i,array.data[i].geo.latitude, array.data[i].geo.longitude)
+		// display more than only the name of the restaurant
+		console.log("restaurant" + i,array.data[i].address_components, array.data[i].address_components)
+
+
+	}
+}
+
+
+var resterauntNames = [ "Uovodiseppia Milano", " Ristorante Rubacuori", "The Pure Cafe", 'A Buon Rendere', 'South Garage Bistrot', 'Ristorante Mezzo'  ]
+
+
+
+
